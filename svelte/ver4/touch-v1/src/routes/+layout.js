b527dbd030f89929d3972fbb066462ee31997f59
j-nielle <toonw48@gmail.com>
2024-02-29 19:30:34 +0800

    vercel adapter

---
+++ svelte/ver4/touch-v1/src/routes/+layout.js
@@ -0,0 +1,13 @@
+import { dev } from '$app/environment';
+
+inject({ mode: dev ? 'development' : 'production' });
+
+/** @type {import('./$types').LayoutLoad} */
+export async function load() {
+    return {};
+}
+
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+	runtime: 'nodejs18.x'
+};
